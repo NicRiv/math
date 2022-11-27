3301e57b36822e8da241541b740204f965d033be
NicRiv <nico.a.rivera3@gmail.com>
2022-11-27 15:36:36 -0300

    Sucesiones

--- sucesiones/sucesiones.js
+++ sucesiones/sucesiones.js
@@ -5,14 +5,14 @@
 // sucesión: an
 const scsLineal = (a) => {
     console.log('\nSucesion: a*n')
-    // const a = 2
+
     let n = 1
     const s = () => a*n
 
     console.log(`[a = ${a}]`)
     while (n <= 10) {
-        const v = s()
-        console.log(`${n} : ${v}`)
+        const res = s()
+        console.log(`${n} : ${res}`)
         n++
     }
 }
