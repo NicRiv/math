f45b6be3b6c4f21c47cbd5dac7c2840419dd8bad
NicRiv <nico.a.rivera3@gmail.com>
2022-11-27 22:34:40 -0300

    lineal

--- sucesiones/sucesiones.js
+++ sucesiones/sucesiones.js
@@ -3,17 +3,17 @@
  */
 
 // sucesión: an
-const scsLineal = (a) => {
+const scsLineal = (a, max) => {
     console.log('\nSucesion: a*n')
 
     let n = 1
     const s = () => a*n
 
     console.log(`[a = ${a}]`)
-    while (n <= 10) {
+    while (n <= max) {
         const res = s()
         console.log(`${n} : ${res}`)
         n++
     }
 }
-scsLineal(3)
+scsLineal(3, 10)
