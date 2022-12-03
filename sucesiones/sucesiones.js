6d342d771341e32aabff3f53adc3f00c68a9fa44
NicRiv <nico.a.rivera3@gmail.com>
2022-12-03 00:51:33 -0300

    Simplificación, y + formulas

--- sucesiones/sucesiones.js
+++ sucesiones/sucesiones.js
@@ -4,97 +4,33 @@
 
 // sucesión: n+a
 const sSum = (a, n) => a+n 
-// const scsSum = (a, max) => {
-//     console.log('\nSucesion: n+a')
-
-//     let n = 1
-//     const s = () => n+a
-
-//     console.log(`[a = ${a}]`)
-//     while (n <= max) {
-//         const res = s()
-//         console.log(`${n} : ${res}`)
-//         n++
-//     }
-// }
-// scsSum(2, 10)
 
 // sucesión: n-a
-const scsResta = (a, max) => {
-    console.log('\nSucesion: n-a')
-
-    let n = 1
-    const s = () => n-a
-
-    console.log(`[a = ${a}]`)
-    while (n <= max) {
-        const res = s()
-        console.log(`${n} : ${res}`)
-        n++
-    }
-}
-// scsResta(2, 10)
+const sResta = (a, n) => a-n
 
 // sucesión: an
-const scsMult = (a, max) => {
-    console.log('\nSucesion: a*n')
-
-    let n = 1
-    const s = () => a*n
-
-    console.log(`[a = ${a}]`)
-    while (n <= max) {
-        const res = s()
-        console.log(`${n} : ${res}`)
-        n++
-    }
-}
-// scsMult(3, 10)
+const sMult = (a, n) => a*n
 
 // sucesión: n/a
-const scsDiv = (a, max) => {
-    console.log('\nSucesion: n/a')
-
-    let n = 1
-    const s = () => n/a
-
-    console.log(`[a = ${a}]`)
-    while (n <= max) {
-        const res = s()
-        console.log(`${n} : ${res}`)
-        n++
-    }
-}
-// scsDiv(2, 10)
+const sDiv = (a, n) => n/a
 
 // sucesión: n^a
-const scsPot = (a, max) => {
-    console.log('\nSucesion: n^a')
-
-    let n = 1
-    const s = () => Math.pow(n, a)
-
-    console.log(`[a = ${a}]`)
-    while (n <= max) {
-        const res = s()
-        console.log(`${n} : ${res}`)
-        n++
-    }
-}
-// scsPot(2, 10)
+const sPot = (a, n) => Math.pow(n, a)
 
 // sucesión: n^(1/a)
-const scsRaiz = (a, max) => {
-    console.log('\nSucesion: n^(1/a)')
+const sRaiz = (a, n) => Math.pow(n, 1/a)
 
-    let n = 1
-    const s = () => Math.pow(n, 1/a)
+// sucesión: a^n
+const sExp = (a, n) => Math.pow(a, n)
 
-    console.log(`[a = ${a}]`)
-    while (n <= max) {
-        const res = s()
-        console.log(`${n} : ${res}`)
-        n++
-    }
-}
-// scsRaiz(2, 10)
+// sucesión: (-1)^n | cambio de signo {imp: -1, par: 1}
+const sSigno = n => Math.pow(-1, n)
+
+// sucesión: (-1)^n * a | cambio de signo {imp: -a, par: a}
+const sAmplitud = (a, n) => Math.pow(-1, n) * a
+
+// sucesión : (-1)^(n + 1) | cambio de signo {imp: 1, par: -1}
+const sSignoInv = n => Math.pow(-1, n + 1)
+
+// sucesión: (-1)^(n + 1) * a | cambio de signo {imp: a, par: -a}
+const sAmplitudInv = n => Math.pow(-1, n + 1) * a
